6d3b19f6a348dc931674f7972afecb5c100ba2b5
Devon Galloway <Devon_Galloway5@hotmail.com>
2019-03-12 21:19:06 -0400

    gifs on page

--- assets/javascript/gif.js
+++ assets/javascript/gif.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
 
     var animals = ["dog" ,"cat" , "rabbit" , "hamster" , "bird" , "snake"];
+    console.log(animals)
 
     function populateButtons(arrayToUse, classToAdd, areaToAddTo) {
         $(areaToAddTo).empty();
@@ -20,15 +21,17 @@ $(document).ready(function() {
         $("#animals").empty();
         $(".animal-button").removeClass("active");
         $(this).addClass("active");
+    
 
         var type = $(this).attr("data-type");
-        var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + type + "&api_key=dc6zaTOxFJmzC&limit=10";
+        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + type + "&api_key=dc6zaTOxFJmzC&limit=10";
 
+        
         $.ajax({
             url: queryURL,
             method: "GET"
         })
-        .then(function(response){
+            .then(function(response){
             var results = response.data;
 
             for (var i = 0; i < results.length; i++) {
@@ -38,8 +41,52 @@ $(document).ready(function() {
                 var rating = results[i].rating;
                 
                 var p = $("<p>").text("Rating: " + rating);
+
+                var animated = results[i].images.fixed_height.url;
+                var still = results[i].images.fixed_height_still.url;
+
+                var animalImage = $("<img>");
+                animalImage.attr("src", still);
+                animalImage.attr("data-still", still);
+                animalImage.attr("data-animate", animated);
+                animalImage.attr("data-state", still);
+                animalImage.addClass("animal-image");
+
+                animalDiv.append(p);
+                animalDiv.append(animalImage);
+
+                $("#animals").append(animalDiv);
                 
-            }
-        })
-    }
-})
+            };
+        });
+    });
+
+
+    $(document).on("click", ".animal-image", function(){
+
+        var state = $(this).attr("data-state");
+
+        if (state === "still") {
+            $(this).attr("src", $(this).attr("data-animate"));
+            $(this).attr("data-state", "animate");
+        }
+        else {
+            $(this).attr("src", $(this).attr("data-still"));
+            $(this).attr("data-state", "still");
+        }
+    });
+    
+    $("#add-animal").on("click", function(event) {
+        event.preventDefault();
+        var newAnimal = $ ("input").eq(0).val();
+        
+        if (newAnimal.length > 2) {
+            animals.push(newAnimal);
+        }
+        
+        populateButtons(animals, "animal-button", "#animal-buttons");
+
+    });
+
+    populateButtons(animals, "animal-button", "#animal-buttons");
+});
